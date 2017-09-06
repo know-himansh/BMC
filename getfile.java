0190f7635b0abec696f0b0192a95be85e27eea14
know-himansh <khatri.himanshu589@gmail.com>
2017-09-06 13:42:42 +0530

    Update getfile.java

--- getfile.java
+++ getfile.java
@@ -1,11 +1,10 @@
+import java.util.*;
+
 class getfile
 {
             public static void main(String args[])
             {
-            Collection<Ref> refs = Git.lsRemoteRepository()
-            .setHeads(true)
-            .setRemote(https://github.com/know-himansh/BMC.git)
-            .call();
+            Collection<Ref> refs = Git.lsRemoteRepository().setHeads(true).setRemote(https://github.com/know-himansh/BMC.git).call();
 
     for (Ref ref : refs) {
         System.out.println("Ref: " + ref);
