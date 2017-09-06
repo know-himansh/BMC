4dc32fec48d7f8f3505712ab20e579c9409b5726
know-himansh <khatri.himanshu589@gmail.com>
2017-09-06 13:57:38 +0530

    Update getfile.java

--- getfile.java
+++ getfile.java
@@ -1,13 +1,43 @@
-import java.util.*;
+import org.kohsuke.github.GHRepository;
 
-class getfile
-{
-            public static void main(String args[])
-            {
-            Collection<Ref> refs = Git.lsRemoteRepository().setHeads(true).setRemote("https://github.com/know-himansh/BMC.git").call();
+import org.kohsuke.github.GHRepository.Contributor;
 
-    for (Ref ref : refs) {
-        System.out.println("Ref: " + ref);
+import org.kohsuke.github.GHUser;
+
+import org.kohsuke.github.GitHub;
+
+
+
+import java.util.Collection;
+
+
+
+public class Foo {
+
+    public static void main(String[] args) throws Exception {
+
+        GitHub gh = GitHub.connect();
+
+        for (Contributor c : gh.getRepository("kohsuke/yo").listContributors()) {
+
+            System.out.println(c);
+
+        }
+
     }
+
+
+
+    private static void testRateLimit() throws Exception {
+
+        GitHub g = GitHub.connectAnonymously();
+
+        for (GHUser u : g.getOrganization("jenkinsci").listMembers()) {
+
+            u.getFollowersCount();
+
+        }
+
     }
-                       }
+
+}
